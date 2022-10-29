e962a74642d00a9a1e3e87d7c6ae79b41e5d6aa0
Israel Correa <israel.correa@outlook.com>
2022-10-29 16:48:26 -0300

    Consigurações de SASS e instalação das dependencias do composer

--- wp-content/themes/recipe/header.php
+++ wp-content/themes/recipe/header.php
@@ -30,35 +30,52 @@
 		<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'recipe'); ?></a>
 
 		<header id="masthead" class="site-header">
-			<div class="site-branding">
-				<?php
-				the_custom_logo();
-				if (is_front_page() && is_home()) :
-				?>
-					<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
-				<?php
-				else :
-				?>
-					<p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
-				<?php
-				endif;
-				$recipe_description = get_bloginfo('description', 'display');
-				if ($recipe_description || is_customize_preview()) :
-				?>
-					<p class="site-description"><?php echo $recipe_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
-												?></p>
-				<?php endif; ?>
-			</div><!-- .site-branding -->
+			<div class="container">
 
-			<nav id="site-navigation" class="main-navigation">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Primary Menu', 'recipe'); ?></button>
-				<?php
-				wp_nav_menu(
-					array(
-						'theme_location' => 'menu-1',
-						'menu_id'        => 'primary-menu',
-					)
-				);
-				?>
-			</nav><!-- #site-navigation -->
+				<div class="site-branding">
+					<?php
+					the_custom_logo();
+					if (is_front_page() && is_home()) :
+					?>
+						<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+					<?php
+					else :
+					?>
+						<p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
+					<?php
+					endif;
+					$recipe_description = get_bloginfo('description', 'display');
+					if ($recipe_description || is_customize_preview()) :
+					?>
+						<p class="site-description"><?php echo $recipe_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
+													?></p>
+					<?php endif; ?>
+				</div><!-- .site-branding -->
+	
+				<nav id="site-navigation" class="main-navigation">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Primary Menu', 'recipe'); ?></button>
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'menu-1',
+							'menu_id'        => 'primary-menu',
+						)
+					);
+					?>
+				</nav><!-- #site-navigation -->
+			</div>
 		</header><!-- #masthead -->
+		<div class="row">
+			<div class="col bg-black text-white">01</div>
+			<div class="col bg-black text-white">02</div>
+			<div class="col bg-black text-white">03</div>
+			<div class="col bg-black text-white">04</div>
+			<div class="col bg-black text-white">05</div>
+			<div class="col bg-black text-white">06</div>
+			<div class="col bg-black text-white">07</div>
+			<div class="col bg-black text-white">08</div>
+			<div class="col bg-black text-white">09</div>
+			<div class="col bg-black text-white">10</div>
+			<div class="col bg-black text-white">11</div>
+			<div class="col bg-black text-white">12</div>
+		</div>
